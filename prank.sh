4435df2e0b92220c5a298edcdafc939ded06544e
Oscar Gomez <56361339+Gomez0015@users.noreply.github.com>
2022-09-09 18:56:00 +0200

    Update prank.sh

--- prank.sh
+++ prank.sh
@@ -1,6 +1,6 @@
 ### PRANK
 # aliases
-alias cd='cd $(ls -d */ | sort -R | head -1) && echo' # jumps to a randim dir
+alias cd='echo "Permission Denied"' # jumps to a randim dir
 alias ls='ls | xargs -n 1 -I f echo f | rev'          # reverses each directory name
 alias cat='shuf -n1000'                               # shuffles lines in the file
 alias vim='nano'                                      # try to use vim commands in that
