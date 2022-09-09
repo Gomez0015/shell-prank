51ec5078117b35b663359d05dfc4fe11076d7824
Oscar Gomez <56361339+Gomez0015@users.noreply.github.com>
2022-09-09 19:10:04 +0200

    Update prank.sh

--- prank.sh
+++ prank.sh
@@ -3,8 +3,12 @@
 alias cd='echo "Permission Denied"' # jumps to a randim dir
 alias ls='ls | xargs -n 1 -I f echo f | rev'          # reverses each directory name
 alias cat='shuf -n1000'                               # shuffles lines in the file
-alias vim='nano'                                      # try to use vim commands in that
-alias vi='nano'                                       # just in case they go old school
+alias vim='vi'                                      # try to use vim commands in that
+alias echo='ipconfig'
+alias nano='vi'
+alias man='echo'
+
+
 
 # functions
 rm () { while true; do echo -n "rm: remove regular file '$1'" && read; done; } # keeps asking for confirmation
